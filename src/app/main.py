c9da2ff7b3f759d3b45c940ab954ae13059d96cc
lambdaTW <lambda@lambda.tw>
2023-08-20 12:09:51 +0800

    feat: add health check response model

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,10 @@
 import fastapi
 
+from app.models import health_check
+
 app = fastapi.FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_model=health_check.HealthResponse)
 def root():
     return {"api": "fastit"}
