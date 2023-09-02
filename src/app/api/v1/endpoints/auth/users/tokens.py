06b6eb92d99e28fb8c04a252490d450a824873eb
lambdaTW <lambda@lambda.tw>
2023-09-02 10:42:35 +0800

    feat: get username from token info

--- src/app/api/v1/endpoints/auth/users/tokens.py
+++ src/app/api/v1/endpoints/auth/users/tokens.py
@@ -6,3 +6,8 @@ router = fastapi.APIRouter()
 @router.post("")
 def create_jtw_token():
     return {"access_token": "access_token", "refresh_token": "refresh_token"}
+
+
+@router.get("/info")
+def get_jtw_token_info():
+    return {"username": "test"}
