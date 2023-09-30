9ae1cd272b6061d1f04465731b598cb447e29d2b
lambdaTW <lambda@lambda.tw>
2023-09-30 14:07:09 +0800

    refactor: move create user as a function to auth.user.create

--- src/tests/test_units/test_users_crud.py
+++ src/tests/test_units/test_users_crud.py
@@ -1,9 +1,9 @@
 import pytest
 import sqlalchemy
-from fastapi import encoders
 from sqlalchemy import future as sqlalchemy_future
 from sqlalchemy.ext import asyncio as sqlalchemy_asyncio
 
+from app.crud import auth as auth_crud
 from app.models import auth as auth_models
 
 
@@ -19,10 +19,8 @@ async def test_create_and_read_user(
         "password": password,
     }
     await db.execute(sqlalchemy.text("select 1"))
-    obj_in_data = encoders.jsonable_encoder(obj_in)
-    user = auth_models.User(**obj_in_data)
-    db.add(user)
-    await db.commit()
+    user = await auth_crud.user.create(db, obj_in)
+    await db.flush()
     user = (
         (
             await db.execute(
